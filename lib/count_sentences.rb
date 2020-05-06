0252269ba80c213efe3ad5a16179981119dbea83
Matt Im <matthew.seiim@gmail.com>
2020-05-06 21:18:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,8 @@ class String
   end
 
   def count_sentences(string)
-    if self.end_with(".")
+    if self.end_with(".") || self.end_with("?") || self.end_with("!")
+      self.split
+      
   end
 end
