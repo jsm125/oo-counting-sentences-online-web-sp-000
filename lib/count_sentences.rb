f0735794402135b49f75a8aebec28cbf31eafda4
Matt Im <matthew.seiim@gmail.com>
2020-05-06 20:46:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,11 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence?(last_punctuation)
+    if self.last_punctuation.end_with? == .
+      true 
+    else
+      false
   end
 
   def question?
